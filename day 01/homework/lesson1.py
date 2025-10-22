cf7fb56a24f7a25de3fad1ef3446fb6a9589112f
vgiorgiecdchdh <giogio35v@gmail.com>
2025-10-22 16:17:18 +0400

    second commit

---
+++ day 01/homework/lesson1.py
@@ -0,0 +1,9 @@
+
+age = 11            
+height = 1.52       
+name = "გიორგი"     
+
+
+print(age)
+print(height)
+print(name)
---
+++ day 01/homework/lesson2.py
@@ -0,0 +1,9 @@
+name=12
+name1=55
+print(name, name1)
+
+print(12+55)
+print(55-12)
+print(12*55)
+print(55/12)
+print(55//12)
