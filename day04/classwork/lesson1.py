801b2430ce6e6e89284d65d92b4e98ddc73740be
vgiorgiecdchdh <giogio35v@gmail.com>
2025-11-28 18:51:03 +0400

    second commit

---
+++ day04/classwork/lesson1.py
@@ -0,0 +1,5 @@
+age=int(input("შეიყვანე მომხმარებლის ასაკი:"))
+if age >= 18:
+    print("success login")
+else:
+    print("failed login")
---
+++ day04/classwork/lesson3.py
@@ -0,0 +1,14 @@
+numbers=[5,2,9,1]
+print("საწყისი სია:", numbers)
+
+
+numbers.append(7)
+print("appeand(7):",  numbers)
+
+
+numbers.sort()
+print("sort():" , numbers)
+
+numbers.reverse()
+print("reverse()" , numbers)
+
